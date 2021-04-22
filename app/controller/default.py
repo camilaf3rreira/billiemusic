0ec022fcb5112ca6dd5fdee40264d2cfeb41838b
Camila <camilas.ferreira@hotmail.com>
2021-04-22 09:53:01 -0300

    fourth commit

--- app/controller/default.py
+++ app/controller/default.py
@@ -12,30 +12,56 @@ class Controller:
 
         if option=="1":
             listaAlbuns=[]
-            listaMusicas=[]
             maisMusica="1"
+            maisAlbum="1"
+            
+            while maisAlbum=="1":
+                infAlbum=menu.menuAlbum()
+                album=Album(infAlbum[0],infAlbum[1],infAlbum[2])
+                menu.clear()
 
-            infAlbum=menu.menuAlbum()
-            album=Album(infAlbum[0],infAlbum[1],infAlbum[2])
+                listaMusicas=[]
+
+                while maisMusica=="1":
+                    infMusica=menu.menuMusica()
+                    musica=Musica(infMusica[0],infMusica[1],infMusica[2])
+                    listaMusicas.append(musica)
+                    maisMusica=menu.continuar()
+                
+                menu.clear()
+    
+                album.addLista(listaMusicas)
+                listaAlbuns.append(album)
+                maisAlbum=menu.continuarAlbum()
+                maisMusica="1"
+
             menu.clear()
-            #listaMusicas=menu.listaMusica()
 
-            while maisMusica=="1":
-                infMusica=menu.menuMusica()
-                musica=Musica(infMusica[0],infMusica[1],infMusica[2])
-                listaMusicas.append(musica)
-                maisMusica=menu.continuar()
+            for album in listaAlbuns:
+                print("Informações do Album:")
+                print("Titulo:", album.tituloDoAlbum)
+                print("Ano:", album.anoDeLancamento)
+                print("Nome Banda:", album.nomeDaBanda)
 
+                for musica in album.listaMusicas:
+                    print("Informações da Música:")
+                    print("Titulo:", musica.tituloDaMusica)
+                    print("Duração:", musica.duracaoDaMusica)
+                    print("Favorita:", musica.favorita)
 
-            album.addLista(listaMusicas) 
-            listaAlbuns.append(album)
-            print("Titulo:", album.tituloDoAlbum)
-            print("Ano:", album.anoDeLancamento)
-            print("Nome Banda:", album.nomeDaBanda)
-            for musica in listaMusicas:
-                print("Titulo:", musica.tituloDaMusica)
-                print("Duração:", musica.duracaoDaMusica)
-                print("Favorita:", musica.favorita)
+        
+        if option=="2":
+            pass
+
+        if option=="3":
+            pass
+
+        if option=="4":
+            pass
+
+        if option=="5":
+            print(">>>>>Fim do sistema BillieMusic! Volte sempre!<<<<<<")
+            exit()
 
 
 
--- app/view/Menu.py
+++ app/view/Menu.py
@@ -1,54 +1,55 @@
 from os import system
-#from time import sleep
 
 class Menu:
 
     def menuInicial (self):
-        print('''>>>>>Bem vindo ao sistema Billie Music!
+        print('''>>>>>Bem vindo ao sistema BillieMusic!<<<<<
         1- Cadastrar Álbum"
         2- Pesquisar Álbum
         3- Pesquisar Música
         4- Gerar Playlist
         5- Sair''')
 
-        option=input(">>>>>Qual é a sua opção:")
+        option=input("Digite o número referente a opção escolhida:")
 
         while(option not in ["1","2","3","4","5"]):
-           option=input("Opção inválida, tente novamente!")
+           option=input("Você digitou uma opção inválida, tente novamente!")
 
-        #sleep(2)  
         return option
 
 
     def menuAlbum (self):
         infAlbum=[]
-       # print(">>>>>Você selecionou a apção 1!")
-        infAlbum.append(input("Digite o Título do Album:"))
+        print(">>>>>Para cadastrar um Álbum<<<<<")
+        infAlbum.append(input("Digite o Título:"))
         infAlbum.append(input("Digite o Ano de Lançamento:"))
         infAlbum.append(input("Digite o Nome da Banda:"))
         return infAlbum
 
     def menuMusica (self):
         infMusica=[]
-        infMusica.append(input("Digite o Título da Música:"))
-        infMusica.append(input("Digite a Duração da Música:"))
-        favorita=input("Essa música é Favorita? Diga 0 para Não e 1 Sim!")
+        print(">>>>>Para cadastrar uma Música<<<<<")
+        infMusica.append(input("Digite o Título:"))
+        infMusica.append(input("Digite a Duração:"))
+        favorita=input("Essa música é Favorita? Digite 0 para Não e 1 Sim!")
 
         while(favorita not in ["0","1"]):
-           favorita=input("Opção inválida, tente novamente!")
+           favorita=input("Você digitou uma opção inválida, tente novamente!")
 
         infMusica.append(favorita)
         return infMusica
 
     def continuar (self):
-        maisMusica=input("Você deseja adicionar mais músicas? Diga 0 para Não e 1 Sim!")
+        maisMusica=input("Você deseja adicionar mais uma <<MÚSICA>>? Diga 0 para Não e 1 Sim!")
         while(maisMusica not in ["0","1"]):
-                maisMusica=input("Opção inválida, tente novamente!")
+                maisMusica=input("Você digitou uma opção inválida, tente novamente!")
         return maisMusica           
 
-    def listaAlbuns(self):
-        pass
-
+    def continuarAlbum(self):
+        maisAlbum=input("Você deseja adicionar mais um <<ÁLBUM>>? Diga 0 para Não e 1 Sim!")
+        while(maisAlbum not in ["0","1"]):
+                maisAlbum=input("Você digitou uma opção inválida, tente novamente!")
+        return maisAlbum
 
     def clear(self):
         system("cls")
